8be44b05aa684cd2bba4243c287b103bd50d6b1b
Irma Frat <irmafraticelli412@gmail.com>
2020-07-06 16:57:46 -0400

    Added my files to continue work sync to github.

---
+++ download_image.py
@@ -0,0 +1,10 @@
+import requests
+import csv
+
+path_american ='/home/irma/Documents/LibraryCloudAPI/American Currency collection_money-JSTOR.xlsx'
+
+def data_csv():
+    with open("American Currency collection_money-JSTOR.xlsx",r) as handler: 
+
+def libcloud_search(urn='urn-3:FHCL:1155043'):
+base_url= "https://api.lib.harvard.edu/v2/items.json?urn="
